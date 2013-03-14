701cbd40f3df87e8524fa4a1caba49532aa66f25
Marius Anghel <mariusanghel@mariusanghel-pc.(none)>
2013-03-14 13:41:05 +0200

    add files

---
+++ src/test/resources/public/js/jasmine-bibliothouris.js
@@ -0,0 +1,71 @@
+
+describe("Courses", function() {
+
+    var init    = false;
+    var APP_URL = 'fakeUrl';
+
+    beforeEach(function(){
+
+        if (!init) {
+			Courses.Init();
+			init = true;
+		}
+	
+    });
+
+    describe("Try to see if Courses is object and is correctly instantiated", function(){
+
+        it("Courses Variable should be object", function(){
+            expect(typeof Courses).toEqual('object');
+        });
+
+        it("Courses.CONFIGS Should not be accessed from outside the object", function(){
+            expect(Courses.CONFIGS).not.toBeDefined();
+        });
+
+        it("Courses.Init() is function and is accessible from outside, also it returns true since the object is initialized", function(){
+            expect(typeof Courses.Init).toEqual('function');
+            expect(Courses.Init()).toBe(true);
+        });
+
+    });
+
+
+    describe("Verify if courses list is correctly loaded via AJAX", function(){
+
+        it("Courses list function should be called", function() {
+
+            spyOn($, "ajax").andCallFake(function(options){
+                options.success();
+            });
+
+            var callBack = jasmine.createSpy();
+
+            Courses.LoadCoursesListFromDb(APP_URL, callBack);
+
+            expect(callBack).toHaveBeenCalled();
+
+        });
+    });
+
+
+    describe("Verify if members list is correctly loaded via AJAX", function(){
+
+        it("Members list function should be called", function() {
+
+            spyOn($, "ajax").andCallFake(function(options){
+                options.success();
+            });
+
+            var callBack = jasmine.createSpy();
+
+            Courses.LoadMembersListFromDb(APP_URL, callBack);
+
+            expect(callBack).toHaveBeenCalled();
+
+        });
+    });
+
+
+});
+
